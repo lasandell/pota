6542ae21270d3dc909d7752ffcf1e2cd80418ffa
Luke Sandell <lasandell@gmail.com>
2025-06-21 19:17:11 -0500

    Change app.js into module

--- app.js
+++ app.js
@@ -267,10 +267,8 @@ async function loadMapForSelectedPark(parkData) {
     }
 }
 
-window.onload = function() {
-    map = L.map('map').setView([39.8283, -98.5795], 4); 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-    initializeParkSelector();
-};
+map = L.map('map').setView([39.8283, -98.5795], 4);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+initializeParkSelector();
